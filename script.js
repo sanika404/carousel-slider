712dab41a6713d8aac95d8b3adf101f5ae1a0818
Sanika <134488939+sanika404@users.noreply.github.com>
2023-10-11 14:44:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+/*const panels=document.querySelectorAll('.panel')
+
+panels.forEach(panel =>
+    {
+        panel.addEventListener('click',() =>{
+            removeEventClasses()
+            panel.classList.add('active')
+        })
+    } )
+
+    function removeActiveClasses(){
+        panels.forEach(panel=>{
+            panel.classList.remove('active')
+              })
+    }*/
+     
+
+
+
+
+
+    const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel=>{
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active');
+
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active')
+          })
+}
